17983ecbbd4a729eae2244a68aa64d88b345cbe4
Erwin van Eijk <erwin.vaneijk@gmail.com>
2010-03-12 21:50:48 +0100

    Added extra tests for weird conditions

--- test/test_stringutils.rb
+++ test/test_stringutils.rb
@@ -2,6 +2,11 @@ require "test/unit"
 require "stringutils"
 
 class TestStringutils < Test::Unit::TestCase
+  def test_nullcase
+    assert_equal "".hexify, ""
+    assert_equal "".dottify, ""
+  end
+
   def test_sanity
     assert "foo".hexify == "666f6f"
     assert "foo".dottify == "foo"
@@ -13,5 +18,11 @@ class TestStringutils < Test::Unit::TestCase
     assert_equal "foo\000\000foo".hexify, "666f6f0000666f6f"
     assert_equal "foo\000\000foo".dottify, "foo..foo"
     assert_equal "bar".hexify, "626172"
+  end
+
+  def test_all_funny
+    assert_equal ("\000"*10).hexify, "00"*10
+    assert_equal ("\x10"*10).hexify, "10"*10
+    assert_equal ("\x10"*10).dottify, "."*10
   end
 end
